fa94b9f826681b6a0f2f9078a2f6a53325f7d789
negative <negative.officiall@gmail.com>
2017-07-03 22:34:25 +0430

    Update to v1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name="tgclient",
     packages=find_packages(),
-    version='0.3',
+    version='1.0',
     description='Telegram Api Client',
     author='Negative',
     author_email='negative.officiall@gmail.com',
@@ -16,5 +16,5 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3'
     ],
-    download_url="https://github.com/negative23/tgclient/archive/0.3.tar.gz"
+    download_url="https://github.com/negative23/tgclient/archive/1.0.tar.gz"
 )
