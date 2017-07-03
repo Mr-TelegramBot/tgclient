2d895b7448c8f4d6bbc66b6a526883650c166b8f
negative <negative.officiall@gmail.com>
2017-07-03 22:11:56 +0430

    v0.2

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 from distutils.core import setup
+import setuptools
 
 
 setup(
     name="tgclient",
-    packages=["tgclient"],
-    version='0.1',
+    packages=["tgclient", "tgclient.utils"],
+    version='0.2',
     description='Telegram Api Client',
     author='Negative',
     author_email='negative.officiall@gmail.com',
     url='https://github.com/negative23/telegram',
-    install_requires=[
-        'six', 'requests'
-    ],
+    install_requires=['six', 'requests'],
     keywords='telegram bot',
     license='GPL2',
     classifiers=[
         'Programming Language :: Python :: 3'
     ],
-    download_url="https://github.com/negative23/tgclient/archive/0.1.tar.gz"
+    download_url="https://github.com/negative23/tgclient/archive/0.2.tar.gz"
 )
