bccc5a2361d682841df30d0a846b92f78705dc2a
negative23 <negative.officiall@gmail.com>
2017-07-04 05:07:42 +0430

    report HTTP error

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name="tgclient",
     packages=find_packages(),
-    version='1.1',
+    version='1.2',
     description='Telegram Api Client',
     author='Negative',
     author_email='negative.officiall@gmail.com',
@@ -15,6 +15,5 @@ setup(
     license='GPL2',
     classifiers=[
         'Programming Language :: Python :: 3'
-    ],
-    download_url="https://github.com/negative23/tgclient/archive/1.1.tar.gz"
+    ]
 )
